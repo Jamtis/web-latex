7a2ecaa0bcd99e7e61f16fbd352b31ba40f6bddc
Nicholas Brandt <nicholas.brandt@inf.ethz.ch>
2022-12-18 21:46:20 +0000

    test

--- dist/web/extension.js
+++ dist/web/extension.js
@@ -24,34 +24,40 @@ __webpack_require__.r(__webpack_exports__);
 class LatexCompiler {
     #pdf_tex = new _texlive_js_pdftex_js__WEBPACK_IMPORTED_MODULE_1__["default"];
     static #path_name_regex = /^(.+)\/(.+?)$/;
-
-    constructor() {
-        const mem_promise = this.#pdf_tex.set_TOTAL_MEMORY(80*1024*1024);
-        (async () => {
-            const r = await mem_promise;
-            debugger;
-            console.log(r);
-        })();
-    }
+    static memory_size = 80*1024*1024;
 
     async addFiles() {
         const files_promise = vscode__WEBPACK_IMPORTED_MODULE_0__.workspace.findFiles('**/*');
         const files = await files_promise;
         for (const {path} of files) {
             try {
-                const document = await vscode__WEBPACK_IMPORTED_MODULE_0__.workspace.openTextDocument(path);
-                const content = document.getText();
+                const content = await this.readTextFile(path);
                 const [, parent_path, file_name] = path.match(this.constructor.#path_name_regex);
                 const promise = this.#pdf_tex.FS_createDataFile(parent_path, file_name, content, true, true);
-                debugger;
-                console.log(promise);
+                console.log(await promise);
             } catch (error) {
                 console.warn(error);
             }
         }
     }
-};
 
+    async compile(main_file = './paper.tex') {
+        const main_source = await this.readTextFile(main_file);
+        await this.setMemorySize(this.memory_size);
+        return await this.#pdf_tex.compile(main_source);
+    }
+
+    async readTextFile(path) {
+        const document = await vscode__WEBPACK_IMPORTED_MODULE_0__.workspace.openTextDocument(path);
+        return document.getText();
+    }
+
+    async setMemorySize(size) {
+        this.memory_size = size;
+        const result_size = await this.#pdf_tex.set_TOTAL_MEMORY(size);
+        return size == result_size;
+    }
+};
 
 /***/ }),
 /* 3 */
--- src/web/LatexCompiler.js
+++ src/web/LatexCompiler.js
@@ -4,30 +4,37 @@ import PDFTeX from './texlive.js/pdftex.js';
 export default class LatexCompiler {
     #pdf_tex = new PDFTeX;
     static #path_name_regex = /^(.+)\/(.+?)$/;
-
-    constructor() {
-        const mem_promise = this.#pdf_tex.set_TOTAL_MEMORY(80*1024*1024);
-        (async () => {
-            const r = await mem_promise;
-            debugger;
-            console.log(r);
-        })();
-    }
+    static memory_size = 80*1024*1024;
 
     async addFiles() {
         const files_promise = workspace.findFiles('**/*');
         const files = await files_promise;
         for (const {path} of files) {
             try {
-                const document = await workspace.openTextDocument(path);
-                const content = document.getText();
+                const content = await this.readTextFile(path);
                 const [, parent_path, file_name] = path.match(this.constructor.#path_name_regex);
                 const promise = this.#pdf_tex.FS_createDataFile(parent_path, file_name, content, true, true);
-                debugger;
-                console.log(promise);
+                console.log(await promise);
             } catch (error) {
                 console.warn(error);
             }
         }
+    }
+
+    async compile(main_file = './paper.tex') {
+        const main_source = await this.readTextFile(main_file);
+        await this.setMemorySize(this.memory_size);
+        return await this.#pdf_tex.compile(main_source);
+    }
+
+    async readTextFile(path) {
+        const document = await workspace.openTextDocument(path);
+        return document.getText();
+    }
+
+    async setMemorySize(size) {
+        this.memory_size = size;
+        const result_size = await this.#pdf_tex.set_TOTAL_MEMORY(size);
+        return size == result_size;
     }
 };
